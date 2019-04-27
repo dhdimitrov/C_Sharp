2bf6efbec1fc566fed7d719ae10621cd5df5cec4
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:41:14 +0300

    Add files via upload

---
+++ Score.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            double bonus = 0;
+            if (num <= 100)
+            {
+                bonus = bonus + 5;
+            }else if (num <= 1000)
+            {
+                bonus = num / 5;
+            }else if (num > 1000)
+            {
+                bonus = num / 10;
+            }
+
+            if (num % 2 == 0)
+            {
+                bonus++;
+            }else if (num % 10 == 5)
+            {
+                bonus = bonus + 2; 
+            }
+            Console.WriteLine(bonus);
+            Console.WriteLine(bonus + num);
+        }
+    }
+}
