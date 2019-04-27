e94ad0119e6552e7f01c43cab0bc4aefd72206f9
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:31:57 +0300

    Add files via upload

---
+++ Sort Array.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp61
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 5, -1, 3, 2, 9, -15 };
+
+            Array.Sort(arr);
+            //Array.Reverse(arr);
+            Console.WriteLine(string.Join(",", arr));
+        }
+    }
+}
