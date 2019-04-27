619ec9cb7788a98d2f65f666f96ef69354f716e5
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:29:54 +0300

    Add files via upload

---
+++ Password guess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "1234";
+            string guess = Console.ReadLine();
+            if(password == guess)
+            {
+                Console.WriteLine("Yes");
+            }else Console.WriteLine("No");
+        }
+    }
+}
