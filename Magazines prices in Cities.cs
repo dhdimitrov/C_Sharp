734e3be09acb1ba51949908777d20847646f8f78
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:41:50 +0300

    Add files via upload

---
+++ Magazines prices in Cities.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            string city = Console.ReadLine();
+            double broi = double.Parse(Console.ReadLine());
+            double price = 0;
+            if(city == "Sofia")
+            {
+                if (product == "coffee")
+                {
+                    //Console.WriteLine("Total : " + 0.50 * broi);
+                    price = 0.5;
+                }
+                else if (product == "water")
+                {
+                    //Console.WriteLine(0.80 * broi);
+                    price = 0.8;
+                }
+                else if (product == "beer")
+                {
+                    //Console.WriteLine("Total : " + 1.20 * broi);
+                    price = 1.2;
+                }
+                else if (product == "sweets")
+                {
+                    //Console.WriteLine("Total : " + 1.45 * broi);
+                    price = 1.45;
+                }
+                else if (product == "peanuts")
+                {
+                    //Console.WriteLine("Total : " + 1.50 * broi);
+                    price = 1.5;
+                }
+            }
+            else if (city == "Varna")
+            {
+                if (product == "coffee")
+                {
+                    //Console.WriteLine("Total : " + 0.50 * broi);
+                    price = 0.45;
+                }
+                else if (product == "water")
+                {
+                    //Console.WriteLine(0.80 * broi);
+                    price = 0.7;
+                }
+                else if (product == "beer")
+                {
+                    //Console.WriteLine("Total : " + 1.20 * broi);
+                    price = 1.1;
+                }
+                else if (product == "sweets")
+                {
+                    //Console.WriteLine("Total : " + 1.45 * broi);
+                    price = 1.35;
+                }
+                else if (product == "peanuts")
+                {
+                    //Console.WriteLine("Total : " + 1.50 * broi);
+                    price = 1.55;
+                }
+            }
+            else if (city == "Plovdiv")
+            {
+                if (product == "coffee")
+                {
+                    //Console.WriteLine("Total : " + 0.50 * broi);
+                    price = 0.4;
+                }
+                else if (product == "water")
+                {
+                    //Console.WriteLine(0.80 * broi);
+                    price = 0.7;
+                }
+                else if (product == "beer")
+                {
+                    //Console.WriteLine("Total : " + 1.20 * broi);
+                    price = 1.15;
+                }
+                else if (product == "sweets")
+                {
+                    //Console.WriteLine("Total : " + 1.45 * broi);
+                    price = 1.30;
+                }
+                else if (product == "peanuts")
+                {
+                    //Console.WriteLine("Total : " + 1.50 * broi);
+                    price = 1.5;
+                }
+            }
+            Console.WriteLine("Total : " + price * broi);
+        }
+    }
+}
---
+++ Meters in other .cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double value = double.Parse(Console.ReadLine());
+            string inputType = Console.ReadLine();
+            string outputType = Console.ReadLine();
+
+            double meters = 0;
+
+            if (inputType == "km")
+            {
+                meters = value / 0.001;
+            }
+            else if (inputType == "mm")
+            {
+                meters = value / 1000;
+            }
+            else if (inputType == "cm")
+            {
+                meters = value / 100;
+            }
+            else if (inputType == "ml")
+            {
+                meters = value / 0.000621371192;
+            }
+            else if (inputType == "in")
+            {
+                meters = value / 39.3700787;
+            }
+            else if (inputType == "ft")
+            {
+                meters = value / 3.2808399;
+            }
+            else if (inputType == "yd")
+            {
+                meters = value / 1.0936133;
+            }
+
+            Console.WriteLine(meters);
+        }
+    }
+}
