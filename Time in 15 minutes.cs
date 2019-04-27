0acfdcdbb4bee3a885fb0c8e52a7dd994901f0cb
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:30:11 +0300

    Add files via upload

---
+++ Time in 15 minutes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minute = int.Parse(Console.ReadLine());
+            int add = minute + 15;
+            if (add >= 60)
+            {
+                hour ++;
+                add = add - 60;
+
+            }
+            if (hour >= 24)
+                hour = hour - 24;
+            Console.WriteLine($"The time in 15 minutes will be {hour}:{add}");
+        }
+    }
+}
