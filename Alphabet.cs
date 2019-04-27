5e17e437313f52d26c10df4aa7a9b7c645e468f1
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:32:16 +0300

    Add files via upload

---
+++ Alphabet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            for (char letter = 'a'; letter <= 122; letter++)
+            {
+                Console.WriteLine(letter);
+            }
+
+        }
+    }
+}
