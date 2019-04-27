2b4902143fa740431859e896262bd54958e7741c
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:34:31 +0300

    Add files via upload

---
+++ 3 numbers check.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            if (a == b && b == c)
+            {
+                Console.WriteLine("YES");
+
+            }else Console.WriteLine("NO");
+
+        }
+    }
+}
