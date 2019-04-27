d16cd5cd265f3b830d0a64c210025f1e7dac1e2e
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:31:38 +0300

    Add files via upload

---
+++ Reverse of Array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int[] num = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = length - 1; i >= 0 ; i--)
+            {
+                Console.WriteLine(num[i]);
+            }
+        }
+    }
+}
