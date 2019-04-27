23dd7701d846d63fb8ce652f90eadafcab171723
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:40:42 +0300

    Add files via upload

---
+++ Word Compere.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = Console.ReadLine();
+            string word2 = Console.ReadLine();
+            if (word1 == word2)
+            {
+                Console.WriteLine("Yes");
+            }
+            else Console.WriteLine("No");
+        }
+    }
+}
