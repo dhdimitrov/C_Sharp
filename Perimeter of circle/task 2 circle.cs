a5e0f5819762e0e5fe31ecc602bea55e3d403a6b
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:03:37 +0300

    Add files via upload

---
+++ Perimeter of circle/task 2 circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(r, 2);
+            double perimeter = 2 * Math.PI * r; 
+            //Console.WriteLine(area);
+
+            Console.WriteLine(Math.Round(area, 2));
+            Console.WriteLine(Math.Round(perimeter, 2));
+
+
+
+
+
+
+
+            
+        }
+    }
+}
