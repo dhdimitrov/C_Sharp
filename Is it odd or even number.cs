a1c98c98d9f05ef1cb5014ba94b57467d910a73a
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:28:03 +0300

    Add files via upload

---
+++ Is it odd or even number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0) { Console.WriteLine("The num is even!!!"); }
+            else { Console.WriteLine("The num is odd"); }
+
+        }
+    }
+}
