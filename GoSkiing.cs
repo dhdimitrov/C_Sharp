3af07d8c8cf070e821c043bbd85c08044487089c
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:31:02 +0300

    Add files via upload

---
+++ GoSkiing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int skiori = int.Parse(Console.ReadLine());
+            int jackets = int.Parse(Console.ReadLine());
+            int kaski = int.Parse(Console.ReadLine());
+            int shoes = int.Parse(Console.ReadLine());
+            double price = 0;
+            price += jackets * 120;
+            price += kaski * 75;
+            price += shoes * 299.99;
+            price = price * skiori;
+            Console.WriteLine(price);
+            price = price + (price * 0.2);
+            Console.WriteLine(price);
+
+            
+        }
+    }
+}
