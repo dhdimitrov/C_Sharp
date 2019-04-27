e8adf4293c62c2bca3e3617eafd44f477f8aaf5d
Daniel Dimitrov <46860664+DannyDimitrov2002@users.noreply.github.com>
2019-04-27 12:28:51 +0300

    Add files via upload

---
+++ Integeer compare.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a > b) { Console.WriteLine(a); }
+            else { Console.WriteLine(b); }
+
+        }
+    }
+}
